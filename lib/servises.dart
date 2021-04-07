6fa100c8fd1e8d260311273631db50217788d36f
HaSaN <mahamudul951@gmail.com>
2021-04-07 23:58:57 +0600

    frist commit

---
+++ lib/servises.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'Model/user.dart';
+
+class Services {
+  static const String url = "https://jsonplaceholder.typicode.com/users";
+
+  static Future<List<User>> getUsers() async {
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        List<User> list = parseUsers(response.body);
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  static List<User> parseUsers(String responseBody) {
+    final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+    return parsed.map<User>((json) => User.fromJson(json)).toList();
+  }
+}
